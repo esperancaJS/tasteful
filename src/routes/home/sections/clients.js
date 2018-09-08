fba381e0ffebe7314750710ad48d99c9adc00f51
coiso <buesimples@gmail.com>
2018-09-08 02:29:29 +0100

    clients

--- src/routes/home/sections/clients.js
+++ src/routes/home/sections/clients.js
@@ -1,6 +1,3 @@
-import style from '../style.scss';
-import Slider from "react-slick";
-
 export default () => (
     <div>
         <div class="container-fluid">
@@ -8,68 +5,34 @@ export default () => (
                 <div class="col-8">
 
                     <h3
-                        class="h1 py-3 text-center"
+                        class="h1 py-3 pt-5 text-center"
                         style={{color: 'white'}}
                     >We have trained people at</h3>
                 
                 </div>
             </div>
-            <div class="row">
+            <div class="row pb-5">
                 <div class="animated-lister">
                     <div class="lister-container">
                         <ul>
-                            {/* {Array.from({length: 12}, (x, i) => ( */}
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/gh.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/g.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/gh.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/g.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/gh.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/g.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/gh.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/g.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/gh.png')}
-                                />
-                            </li>
-                            <li>
-                                <img
-                                    src={require('../../../assets/clients/g.png')}
-                                />
-                            </li>
-                            {/* ))} */}
+                            {[
+                                { img: 'gh.png' },
+                                // { img: 'g.png' },
+                                { img: 'beamery.svg' },
+                                { img: 'ibm5.png' },
+                                { img: 'ua.png', maxHeight: '60px' },
+                                { img: 'od.png', maxHeight: '60px' },
+                                { img: 'sony.png' },
+                                { img: 'iw.png', maxHeight: '65px' },
+                                { img: 'altar-37.png' }
+                            ].map(({img, maxHeight}) => (
+                                <li>
+                                    <img
+                                        style={{ maxHeight }}
+                                        src={require(`../../../assets/clients/${img}`)}
+                                    />
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
